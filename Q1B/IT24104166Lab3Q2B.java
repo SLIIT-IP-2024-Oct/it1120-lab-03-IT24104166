7cf30646451cf298468ed0c7616eb48da02bf4fb
IT24104166 <it24104166@my.sliit.lk>
2024-11-01 09:39:22 +0530

    Add files via upload

---
+++ Q1B/IT24104166Lab3Q2B.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lab3Q1{
+  public static voidmain(String[] args){
+ 
+  //declare variables
+  double price of 1kg of rice,number of kilograms you need to buy,Total amount with 10% discount;
+
+  Scanner input=new Scanner(System.in)
+  
+  //enter the price of one kilogram rice and the number of kilograms to buy as user input
+  System.out.println("Enter the prince of 1kg of rice:");
+  price of 1kg of rice=input.nextDouble();
+  System.out.println("Enter the number of kilograms you want to buy:"); 
+  number of kilograms you want to buy=input.nextdouble();
+
+  //calculations
+  Total amount with 10% discount is=price of 1kg of rice*number of kilograms you want to buy*0.1;
+  
+  //Display output
+  System.out.println("Total amount with 10% discount is:"+Total amount with 10% discount is);
+  }
+}
